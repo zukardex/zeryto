e0036178d2867429d9cca254dc802ddeccddf3f0
zukardex <zenzenram@gmail.com>
2022-12-05 22:25:26 +0530

    Add files via upload

--- decrypt.php
+++ decrypt.php
@@ -1,125 +1,126 @@
+<center>
 <?php
-$alph= array('m','l', 'n', 'k', 'o', 'j', 'p', 'i', 'q', 'h', 'r', 'g', 's', 'f', 't', 'e', 'u', 'd', 'v', 'c', 'w', 'b', 'x', 'a', 'y', 'z', '0', '1' , '2', '3', '4', '5', '6', '7', '8', '9', 'M', 'L', 'N', 'K', 'O', 'J', 'P', 'I', 'Q', 'H', 'R', 'G', 'S', 'F', 'T', 'E', 'U', 'D', 'V', 'C', 'W', 'B', 'X', 'A', 'Y', 'Z', '.', ',', ';', '?', '!', ' ');
-$beta=array('p', 'W', 'G', 's', 'R', 'n', 'B', 'H', 'k', 'J');
-function beta_dec($enc, $lensq){
-	GLOBAL $beta;
-	$dec='';
-	$enc_str='';
+	$alph= array('m','l', 'n', 'k', 'o', 'j', 'p', 'i', 'q', 'h', 'r', 'g', 's', 'f', 't', 'e', 'u', 'd', 'v', 'c', 'w', 'b', 'x', 'a', 'y', 'z', '0', '1' , '2', '3', '4', '5', '6', '7', '8', '9', 'M', 'L', 'N', 'K', 'O', 'J', 'P', 'I', 'Q', 'H', 'R', 'G', 'S', 'F', 'T', 'E', 'U', 'D', 'V', 'C', 'W', 'B', 'X', 'A', 'Y', 'Z', '.', ',', ';', '?', '!', ' ');
+	$beta=array('p', 'W', 'G', 's', 'R', 'n', 'B', 'H', 'k', 'J');
+	function beta_dec($enc, $lensq){
+		GLOBAL $beta;
+		$dec='';
+		$enc_str='';
 	
-	for($i=0; $i<count($enc); $i++){
-		for($j=0; $j<count($beta); $j++){
-			if($enc[$i] == 'f'){
-				$enc[$i] = ' '; //NOTE
-			}else if($enc[$i] == $beta[$j]){
-				//if(($i* $j) % 7 ==0){
-					$enc[$i] = $j;
-				//}
+		for($i=0; $i<count($enc); $i++){
+			for($j=0; $j<count($beta); $j++){
+				if($enc[$i] == 'f'){
+					$enc[$i] = ' '; //NOTE
+				}else if($enc[$i] == $beta[$j]){
+					//if(($i* $j) % 7 ==0){
+						$enc[$i] = $j;
+					//}
+				}
 			}
+			$enc_str = $enc_str . $enc[$i];
 		}
-		$enc_str = $enc_str . $enc[$i];
+		//echo $enc_str . '<p>';
+		$enc= explode(' ', $enc_str);
+		return $enc;
 	}
-	echo $enc_str . '<p>';
-	$enc= explode(' ', $enc_str);
-	return $enc;
-}
-function sinz($key_str, $plaintemp, $key){
-	$four_s=0;
-	$key_sum=0;
-	$cube_sum=0;
+	function sinz($key_str, $plaintemp, $key){
+		$four_s=0;
+		$key_sum=0;
+		$cube_sum=0;
 	
-	for($i=0; $i<strlen($key_str); $i++){
-			if($i <4){
-				$four_s+= $key_str[$i]; //four digit sum
-				$cube_sum+= ($key_str[$i] * $key_str[$i] * $key_str[$i] );
+		for($i=0; $i<strlen($key_str); $i++){
+				if($i <4){
+					$four_s+= $key_str[$i]; //four digit sum
+					$cube_sum+= ($key_str[$i] * $key_str[$i] * $key_str[$i] );
+				}
+				$key_sum += $key_str[$i];
 			}
-			$key_sum += $key_str[$i];
-		}
-		$temp_sin=(string) sin(M_PI *($four_s * $cube_sum/90) / 180);
-		$int_sin= (int) $temp_sin;
-		for($i=0; $int_sin<1; $i++){
-			$temp_sin *=10;
+			$temp_sin=(string) sin(M_PI *($four_s * $cube_sum/90) / 180);
 			$int_sin= (int) $temp_sin;
-		}
+			for($i=0; $int_sin<1; $i++){
+				$temp_sin *=10;
+				$int_sin= (int) $temp_sin;
+			}
 		//echo $temp_sin;
 		
 		
 				//echo  '<p>ze=' . $temp_sin . ' ';
-		$z= ($int_sin * $key)/$plaintemp;
+			$z= ($int_sin * $key)/$plaintemp;
 			//echo  '<p>ze=' . $int_sin . ' ';
-			return $z;
-}
-
-function dec($enc, $key, $z_set){
-	GLOBAL $alph;
-	GLOBAL $beta;
-	$key_str= (string) $key;
-	$pos=0;
-	$z=0;
-	$neo_enc=array();
-	$result='';
-	$plaintemp='';
-	
-	
-	$ldig= $enc[-1]; //last digit of encrypted text
-		for($i=0; $i < count($alph); $i++){
-			if($ldig == $alph[$i]){
-				$pos= $i - 1; //$pos=strlen(strlen(PLAIN_TEXT))
-			}
-		}
-		for($i=strlen($enc)- $pos-1; $i<strlen($enc)-1; $i++){
-			$plaintemp = $plaintemp . $enc[$i];
-			//echo  '<p>plain=' . $plaintemp . ' ';
-		}
-		$plaintemp=(int)$plaintemp;
-	
-	if( $z_set == FALSE){
-		$z= sinz($key_str, $plaintemp, $key);
-		$z=(int) $z;
+				return $z;
 	}
-	else{
-		for($i=0; $i<strlen($key_str); $i++){
-			for($j=0; $j<count($beta); $j++){
-				if($key_str[$i] == $beta[$j]){
-					$key_str[$i] = $j;
+
+	function dec($enc, $key, $z_set){
+		GLOBAL $alph;
+		GLOBAL $beta;
+		$key_str= (string) $key;
+			$pos=0;
+		$z=0;
+		$neo_enc=array();
+		$result='';
+		$plaintemp='';
+	
+	
+		$ldig= $enc[-1]; //last digit of encrypted text
+			for($i=0; $i < count($alph); $i++){
+				if($ldig == $alph[$i]){
+					$pos= $i - 1; //$pos=strlen(strlen(PLAIN_TEXT))
 				}
 			}
-		}
-		echo $key_str;
-		$z= (int) $key_str;
-	}
-	echo '<p> $z=' . $z . "<p>";
-
-	//deletes the last ($pos+1) letters from $enc and forms a new array
-	for($i=0; $i < strlen($enc)- $pos-1; $i++){
-		$neo_enc[$i] = $enc[$i];
-	}
-	$alen= beta_dec($neo_enc, $pos);
+			for($i=strlen($enc)- $pos-1; $i<strlen($enc)-1; $i++){
+				$plaintemp = $plaintemp . $enc[$i];
+				//echo  '<p>plain=' . $plaintemp . ' ';
+			}
+			$plaintemp=(int)$plaintemp;
 	
-	//Key deciphering and alpha decoding
-	for($i=0; $i<count($alen); $i++){
-		if($alen[$i] !=0){
-			$alen[$i] = $alen[$i] / $z;
-			for($j=0; $j<count($alph); $j++){
-				if($alen[$i] == $j){
-					$alen[$i]= $alph[$j];
+		if( $z_set == FALSE){
+			$z= sinz($key_str, $plaintemp, $key);
+			$z=(int) $z;
+		}
+		else{
+			for($i=0; $i<strlen($key_str); $i++){
+				for($j=0; $j<count($beta); $j++){
+					if($key_str[$i] == $beta[$j]){
+						$key_str[$i] = $j;
+					}
 				}
 			}
-		}else{
-			$alen[$i] = 'm';
+			//echo $key_str;
+			$z= (int) $key_str;
 		}
-		if($i != count($alen)-1){
-			$result =$result . $alen[$i];
+			//echo '<p> $z=' . $z . "<p>";
+
+		//deletes the last ($pos+1) letters from $enc and forms a new array
+		for($i=0; $i < strlen($enc)- $pos-1; $i++){
+			$neo_enc[$i] = $enc[$i];
 		}
-		//echo '<p>' . $alen[$i];
-	}
-	return $result;
+		$alen= beta_dec($neo_enc, $pos);
+	
+		//Key deciphering and alpha decoding
+		for($i=0; $i<count($alen); $i++){
+			if($alen[$i] !=0){
+				$alen[$i] = $alen[$i] / $z;
+				for($j=0; $j<count($alph); $j++){
+					if($alen[$i] == $j){
+						$alen[$i]= $alph[$j];
+					}
+				}
+			}else{
+				$alen[$i] = 'm';
+			}
+			if($i != count($alen)-1){
+				$result =$result . $alen[$i];
+			}
+			//echo '<p>' . $alen[$i];
+		}
+		return $result;
 	
 }
 
 
 
 
-
+/*
 if(isset($_GET['text']) ){
 	$t=$_GET['text'];
 	if(isset($_GET['z'])){
@@ -135,11 +136,64 @@ if(isset($_GET['text']) ){
 			echo dec($t, $key, false);
 		}
 	}} 
-}
+}*/
+
 ?>
-<form method="get">
-<input type="text" name="text" placeholder="Text to decrypt"/>
-<input type="text" name="key" placeholder="Encryption key"/>
-<input type="text" name="z" placeholder="Private $z key"/>
-<input type="submit" name="submit"/>
-</form>
+
+
+<?php
+if(isset($_GET['nIn'])){
+	$zeryto12d_nIn= $_GET['nIn'];
+	$zeryto12d_nin= (int)$zeryto12d_nIn;
+	
+	echo '<form method="post">';
+	echo '<input type="text" placeholder="Text to decrypt" name="dec_1" value="471nJJ0Bn0k0ff0GGf0BW0580B0ff0WW0WRfGJ2nRnff0Gs0kk07W0nk0ff00W0f100B0fG0Wf0kW07B0nB0ff0G60Wn0G00n00f00800n0003"><br><p>';
+	for($zeryto12d_i=0; $zeryto12d_i <$zeryto12d_nIn; $zeryto12d_i++){
+		echo '<input type="key" name="pass_'.($zeryto12d_i+1) .'" value="1000" placeholder="Encryption Key ' . ($zeryto12d_i+1). '"><br>';
+	}
+	
+	echo '<input type="text" name="z" placeholder="private $z key" value="162314432993"/><br><input type="submit" name="submit"/> ';
+	echo '</form>';
+	
+	
+if(isset($_POST['dec_1']) && isset($_POST['pass_' . $zeryto12d_nIn])){
+	//get the number of plaintexts
+	$zeryto12d_rem= $_POST['dec_1'];
+	$zeryto12d_rem=(int) $zeryto12d_rem[strlen($zeryto12d_rem)-1];
+	$zeryto12d_div=$_POST['dec_1'];
+	$zeryto12_po=(int) $zeryto12d_div[strlen($zeryto12d_div) -2]; 
+	$zeryto12d_div=pow(32,  $zeryto12_po) -1; //echo 'rem='. $zeryto12d_rem . ' div=' . $zeryto12d_div . '<p>';
+	$zeryto12d_div = $zeryto12d_div + $zeryto12d_rem ; //This contains the number of plaintexts it really includes.
+	//echo $zeryto12d_div;
+	
+	//Divide div by the strlen of ciphertext to get the exact ssize of the largest plaintext.
+	$zeryto12_len= (strlen($_POST['dec_1']) - 2)/ $zeryto12d_div;
+	//echo '<p>' . $zeryto12_len;		//Success this includes the size of all the plaintexts
+	
+	//gamma decoding of the z value and getting accaess to the zmix.
+	$zeryto12_gamma=array('k', 'W', 'n','7', 'B', 'H', 'G', 's', 'R','p', 'J');
+	$zeryto12d_zg= $_POST['z']; 
+	for($zeryto12d_i=0; $zeryto12d_i < strlen($zeryto12d_zg); $zeryto12d_i++){
+		for($zeryto12d_j=0; $zeryto12d_j < count($zeryto12_gamma); $zeryto12d_j++){
+			if($zeryto12d_zg [$zeryto12d_i] == $zeryto12d_j){
+				$zeryto12d_zg [$zeryto12d_i] = $zeryto12_gamma[$zeryto12d_j];
+			}
+		}
+	}
+	echo ' zmix= ' . $zeryto12d_zg;
+	
+}
+	
+	
+//ZERTO 1.2
+}else{
+	echo '<form method ="get" >
+	<input type="number_format" name="nIn" placeholder="Number of inputs you wish to take "><br>
+	<input type="submit" name="submit">
+</form>';
+}
+
+
+?>
+
+</center>
--- zeryto-1.21 (stable).php
+++ zeryto-1.21 (stable).php
@@ -1,7 +1,12 @@
+<center>
+<!---												ZERYTO 1.2  				-->
+
+<!--Section ZERYTO 1.1 -->
 <?php
 //include 'decrypt.php';
 $zeryto11_alph= array('m','l', 'n', 'k', 'o', 'j', 'p', 'i', 'q', 'h', 'r', 'g', 's', 'f', 't', 'e', 'u', 'd', 'v', 'c', 'w', 'b', 'x', 'a', 'y', 'z', '0', '1' , '2', '3', '4', '5', '6', '7', '8', '9', 'M', 'L', 'N', 'K', 'O', 'J', 'P', 'I', 'Q', 'H', 'R', 'G', 'S', 'F', 'T', 'E', 'U', 'D', 'V', 'C', 'W', 'B', 'X', 'A', 'Y', 'Z', '.', ',', ';', '?', '!', ' ');
 $zeryto11_beta=array('p', 'W', 'G', 's', 'R', 'n', 'B', 'H', 'k', 'J');
+$zeryto12_gamma=array('k', 'W', 'n','7', 'B', 'H', 'G', 's', 'R','p', 'J');
 function beta_enc($zeryto11_txt, $zeryto11_art){
 	GLOBAL $zeryto11_beta;
 	for($zeryto11_i=0; $zeryto11_i<strlen($zeryto11_txt); $zeryto11_i++){
@@ -82,7 +87,7 @@ function enc($zeryto11_a, $zeryto11_key){
 
 //MAKE $zeryto11_modulo value variable along with strlen($zeryto11_a) to get randomized results
 	$zeryto11_modulo= ($zeryto11_keysf * $zeryto11_keyscub) / 90; //$zeryto11_modulo= ($zeryto11_keysf * $zeryto11_keyscub) / (90*$zeryto11_len_mod);
-	echo $zeryto11_key;
+	//echo $zeryto11_key;
 	$zeryto11_temp_sin= sin(M_PI * $zeryto11_modulo/(180));
 	//echo $zeryto11_temp_sin;
 	$zeryto11_int_sin= (int)$zeryto11_temp_sin;
@@ -116,10 +121,10 @@ function enc($zeryto11_a, $zeryto11_key){
 			}
 		}
 */
-	echo '<p>plain=' . strlen($zeryto11_a);
+	//echo '<p>plain=' . strlen($zeryto11_a);
 	$zeryto11_z_temp= ($zeryto11_int_sin * $zeryto11_key)/strlen($zeryto11_a);
 	$zeryto11_z=(int) $zeryto11_z_temp;
-	echo "<p>z=" . $zeryto11_z . "<p>";
+	//echo "<p>z=" . $zeryto11_z . "<p>";
 	
 	/*
 		1)Create a loop which runs from 0 to strlen($zeryto11_a) and access each letter of $zeryto11_a seperately
@@ -144,17 +149,20 @@ function enc($zeryto11_a, $zeryto11_key){
 		}
 	} 
 	if($zeryto11_m == count($zeryto11_enc_digt)){
-		echo "<b>Encryption process was done successfully</b><p>";
+		//echo "<b>Encryption process was done successfully</b><p>";
 	}
 	//convert the array into  a string
 	for($zeryto11_j=0; $zeryto11_j < count($zeryto11_enc_digt); $zeryto11_j++){
 		$zeryto11_result= $zeryto11_result . $zeryto11_enc_digt[$zeryto11_j] . ' ';
 	}
 	if($zeryto11_result != ''){
-		echo ' <p>$zeryto11_result='. $zeryto11_result . "<p>";
-		//return salt($zeryto11_result, strlen($zeryto11_a));
-		echo "final=" . salt($zeryto11_result, strlen($zeryto11_a)) . '<p>';
-		echo "<p>\$zeryto11_z private key= " . enc_z($zeryto11_z) . "<p>";
+		//echo ' <p>$zeryto11_result='. $zeryto11_result . "<p>"; 				//Modified
+				////return salt($zeryto11_result, strlen($zeryto11_a));
+				//echo "<p>\$z private key= " . enc_z($zeryto11_z) . "<p>";	//Modified
+				$zeryto12_saltd =salt($zeryto11_result, strlen($zeryto11_a)); 
+				$zeryto12_zrev= array($zeryto12_saltd, enc_z($zeryto11_z)); //returning array
+		return 	$zeryto12_zrev;				//Modified
+		
 	}else{
 		echo 'Encryption failed';
 	}
@@ -177,3 +185,113 @@ function enc($zeryto11_a, $zeryto11_key){
 */
 
 ?>
+
+<!--Section ZERYTO 1.2 -->
+<?php
+
+/*
+				<<<ZERYTO 1.2 - One Cipher, Many key, Many plaintexts>>>
+*/
+
+if(isset($_GET['nIn'])){
+	$zeryto12_nIn= $_GET['nIn'];
+	$zeryto12_nin= (int)$zeryto12_nIn;
+	
+	echo '<form method="post">';
+	for($zeryto12_i=0; $zeryto12_i <$zeryto12_nIn; $zeryto12_i++){
+		echo '<input type="text" placeholder="Text to encrypt ' . ($zeryto12_i+1) . '" name="enc_' . ($zeryto12_i+1) . '"><input type="key" name="pass_'.($zeryto12_i+1) .'" placeholder="Encryption Key ' . ($zeryto12_i+1). '"><br>';
+	}
+	echo '<input type="submit" name="submit"/> ';
+	echo '</form>';
+	
+	
+	
+	if(isset($_POST['enc_' . $zeryto12_nIn]) && isset($_POST['pass_' . $zeryto12_nIn])){
+		//Encryption Processes proceeding
+	
+		//1.Find the size of each plaintext and collect them into an array.
+		//2.Find which plaintext is the biggest and make all the smaller plaintexts with the same size of the biggest's by adding some zeros to it.
+	
+		$zeryto12_sizes=array();
+		$zeryto12_temp=0;
+		//$zeryto12_key = $_POST['key'];
+		$zeryto12_encd=array(); //Encrypted individual plaintext
+		$zeryto12_encd2=array(); //z values
+		for($zeryto12_i=0; $zeryto12_i<$zeryto12_nin; $zeryto12_i++){
+			//encrypt each plaintext and accept the z values and save it into two different arrays.
+			$zeryto12_modif= enc($_POST['enc_' . ($zeryto12_i +1)], $_POST['pass_' . ($zeryto12_i +1)]);
+			$zeryto12_encd[count($zeryto12_encd)]= $zeryto12_modif[0]; //contains the encrypted texts
+			$zeryto12_encd2[count($zeryto12_encd2)]= $zeryto12_modif[1]; //contains the z values
+			
+			
+			
+			
+			$zeryto12_sizes[$zeryto12_i] = strlen($zeryto12_modif[0]);	
+			if($zeryto12_sizes[$zeryto12_i] > $zeryto12_sizes[$zeryto12_temp]){
+				$zeryto12_temp=$zeryto12_i;
+			}
+		} //$zeryto12_temp+1 is the biggest
+		
+		$zeryto12_univar=array();
+		for($zeryto12_i=0; $zeryto12_i<$zeryto12_nIn; $zeryto12_i++){
+			$zeryto12_univar[$zeryto12_i]='';
+			for($zeryto12_j=0; $zeryto12_j< $zeryto12_sizes[$zeryto12_temp]; $zeryto12_j++){
+				//Process every character individually and add whitespaces if the size is deficit.
+				if($zeryto12_j < $zeryto12_sizes[$zeryto12_i]){
+					$zeryto12_txr= $zeryto12_encd[($zeryto12_i)];
+					$zeryto12_univar[$zeryto12_i]= $zeryto12_univar[$zeryto12_i] . $zeryto12_txr[$zeryto12_j];
+				}else{
+					$zeryto12_univar[$zeryto12_i]= $zeryto12_univar[$zeryto12_i] . '0';
+				}
+			}
+		} //Now univar array contains all the modified plaintexts.
+
+		//Salt the plaintexts and submit it for encryption process.
+		$zeryto12_salard='';
+			for($zeryto12_j=0; $zeryto12_j<$zeryto12_sizes[$zeryto12_temp]; $zeryto12_j++){
+				for($zeryto12_k=0; $zeryto12_k<$zeryto12_nin; $zeryto12_k++){
+					$zeryto12_salard= $zeryto12_salard. $zeryto12_univar[$zeryto12_k][$zeryto12_j];
+				}
+			}
+			//echo $zeryto12_salard . " hihi<p>";
+		//$zeryto12_salard = enc($zeryto12_salard, $zeryto12_key);
+		//After encryption, attach the number of the inputs with base to 32 and concatenate it with ($nin % 32)
+		$zeryto12_nIn32= (int) ($zeryto12_nIn /32);
+		$zeryto12_salard= $zeryto12_salard . ($zeryto12_nIn32) . ($zeryto12_nin %32); //Success. This variable contains the plain text mix
+		
+		
+		// $z value mix
+		$zeryto12_zmix='';
+		for($zeryto12_i=0; $zeryto12_i <$zeryto12_nIn; $zeryto12_i++){
+			$zeryto12_zmix= $zeryto12_zmix . $zeryto12_encd2[$zeryto12_i] . '7'  ;
+		}
+		// zmix gamma coding
+		global $zeryto12_gamma;
+		for($zeryto12_i=0; $zeryto12_i <strlen($zeryto12_zmix); $zeryto12_i++){
+			for($zeryto12_j=0; $zeryto12_j <count($zeryto12_gamma); $zeryto12_j++){
+				if($zeryto12_gamma[$zeryto12_j] == $zeryto12_zmix[$zeryto12_i]){
+					$zeryto12_zmix[$zeryto12_i] = $zeryto12_j;
+				}
+			} //alph(zmix gamma mix)
+			for($zeryto12_k=0;$zeryto12_k <count($zeryto11_alph); $zeryto12_k++){
+				if($zeryto12_k  == $zeryto12_zmix[$zeryto12_i]){
+					$zeryto12_zmix[$zeryto12_i] = $zeryto11_alph[$zeryto12_k];
+				}
+			}
+		}
+		echo $zeryto12_zmix; //This contains the z value mix
+
+	}
+	
+
+}else{
+	echo '<form method ="get" >
+	<input type="number_format" name="nIn" placeholder="Number of inputs you wish to take "><br>
+	<input type="submit" name="submit">
+</form>';
+}
+
+
+?>
+
+</center>
